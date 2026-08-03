95ca52274d0bc572e000975f95c4ee3f424424b8
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-03 18:14:33 +0700

    feat: add Hello World (TypeScript)

---
+++ programs/hello_world.ts
@@ -0,0 +1,6 @@
+// hello_world.ts
+// Program pertama dengan TypeScript.
+// Compile: npx tsc hello_world.ts && node hello_world.js
+
+const name: string = 'World';
+console.log('Hello, ' + name + '!');
